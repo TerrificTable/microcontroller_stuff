40631f87eead20c051893ad4b4ce109e0cc1796a
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-05-11 12:08:16 +0200

    Create a WiFi Access Point using a ESP-8266

--- WiFi_AP.cpp
+++ WiFi_AP.cpp
@@ -5,74 +5,6 @@
 
 #include <ESP8266WiFi.h>
 
-// for wifi to Connect to
-// ===
-#ifndef STASSID
-#define STASSID         "WIFI NAME"
-#define STAPSK          "WIFI PASSWORD"
-#endif
-
-const char *ssid            = STASSID;
-const char *password        = STAPSK;
-// ===
-
-typedef struct {
-    boolean         WLANConnected               = false;  
-    boolean         WLANConnecting              = false;  
-    unsigned long   WLANConnectingTimeOut       = 0;  
-    unsigned long   WLANConnectingWaitTimeOut   = 0;  
-} StatesData;
-StatesData States;
-
-
-WiFiClient          espClient;
-
-
-
-
-
-
-void WIFI_Connect() {
-    // Statemachine so it can run withouht freezing the esp
-    
-    // Connect to Wifi
-    if (!States.WLANConnected &&  WiFi.status() == WL_CONNECTED) {
-        States.WLANConnecting = false;
-        States.WLANConnectingWaitTimeOut = 0;
-        Serial.print("WiFi Connected: ");
-        String IP = WiFi.localIP().toString();
-
-        Serial.println(IP);
-    }
-
-    States.WLANConnected = WiFi.status() == WL_CONNECTED || WiFi.status()  == WL_IDLE_STATUS;
-    
-    if (States.WLANConnected) return;  
-    
-    
-    if (States.WLANConnectingWaitTimeOut > 0 && millis() < States.WLANConnectingWaitTimeOut ) return;
-    if (States.WLANConnectingWaitTimeOut > 0 && millis() > States.WLANConnectingWaitTimeOut ) States.WLANConnectingWaitTimeOut = 0;
-
-
-    if (!States.WLANConnecting) {
-        States.WLANConnecting = true;
-        States.WLANConnectingTimeOut = millis() + 10000;  // 10 Sekunfen
-        Serial.println("WiFi connecting....");
-        
-        WiFi.persistent(false); 
-        WiFi.mode( WIFI_OFF );
-        delay( 1 );
-        WiFi.disconnect();
-        WiFi.begin(ssid, password);
-        return;
-    }
-
-    if (States.WLANConnecting &&  millis() > States.WLANConnectingTimeOut) {
-        States.WLANConnecting  = false;
-        Serial.println("WiFi connecting failed....");
-        States.WLANConnectingWaitTimeOut = millis() + 10000;
-    }
-}
 
 
 
@@ -90,6 +22,5 @@ void setup() {
 
 
 void loop() {
-    // Connect to WIFI so if you connect to the esp wifi you will acutally get a internet connection
-    WIFI_Connect();
+    // Without this delcared it gives me a lot of errors, and since this script doesnt need it there is nothing here
 }
